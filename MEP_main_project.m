df823bfe760e0ee9a19f42d99f1533de689b0220
maria.zamyatina <maria.zamyatina.int@gmail.com>
2014-11-24 09:35:06 +0000

    MIE scheme is rewritten for all species

--- MEP_main_project.m
+++ MEP_main_project.m
@@ -1,21 +1,22 @@
-clear;clc;close
+%clear;clc;close
 %% Model parameters
-t0 = 0;
-nt = 400; % simulation time
-dt = 10; % time step [s]
-nsteps = numel(t0:dt:nt);
-T = 298; % absolute temperature [Kelvins], +25 C
-p = 920; % total air pressure [hPa]
+T = 298.; % absolute temperature [Kelvins], +25 C
+p = 920.; % total air pressure [hPa]
 kB = 1.380658*10^(-19); % Boltzmann's constant [cm-3 hPa K-1 molec-1]
-scheme = 3; % 1 - Forward Euler (nt = 1 s, dt = 10^(-4) s); 2 - Backward Euler (nt = 2000 s, dt = 10 s); 3 - MIE
+t0 = 0;
+nt = 2000; % simulation time [s]
+dt = 10; % time step [s]
+nsteps = numel(t0:dt:nt); % number of time steps
+scheme = 2; % 1 - Forward Euler (nt = 1 s, dt = 10^(-4) s); 2 - Backward Euler (nt = 2000 s, dt = 10 s); 3 - MIE
 Np = 30; % 5 for large set of reastions, 30-50 for small set of reactions
+itermax = 100; % empirically derived reosanoble ilimit of number of iterations
 %% Initial concentrations
-O3 = 0; % molec cm-3
+O3 = 0.; % molec cm-3
 M = p/(kB*T);
 O2 = 0.2095*M; % assuming mixiting ratio of O2 = 0.2095 and air is dry
-O = 0;
-NO = 10^(12);
-NO2 = 10^(10);
+O = 0.;
+NO = 1e12;
+NO2 = 1e10;
 %% Rate coefficients
 k1 = 1.8*10^(-12)*exp(-1370/T); % NO+O3 -> NO2+O2
 J = 1.7*10^(-2);                        % NO2+hv -> NO+O, <420 nm
@@ -41,7 +42,7 @@ switch scheme
             Oforw(i+1) = Oforw(i) + dt*(J*NO2forw(i) - k3*Oforw(i));
             O3forw(i+1) = O3forw(i)+dt*(k3*Oforw(i) - k1*NOforw(i)*O3forw(i));
         end
-        case 2 % Backward Euler
+    case 2 % Backward Euler
             % Preallocating space for variables
             O3back(1:nsteps) = NaN; Oback(1:nsteps) = NaN; NOback(1:nsteps) = NaN; NO2back(1:nsteps) = NaN;
             O3back(1) = O3(1); Oback(1) = O(1); NOback(1) = NO(1); NO2back(1) = NO2(1);
@@ -53,58 +54,71 @@ switch scheme
                 Oback(i+1) = (Oback(i) + dt*J*NO2back(i))/(1 + dt*k3);
                 O3back(i+1) = (O3back(i) + dt*k3*Oback(i))/(1 + dt*k1*NOback(i));
             end
-    case 3 % MIE
+    case 3 % Multistep implicit–explicit (MIE)
         % Preallocating space for variables
-        O3mie(1:nsteps) = NaN; Omie(1:nsteps) = NaN; NOmie(1:nsteps) = NaN; NO2mie(1:nsteps) = NaN;
-%         O3mieback(1:nsteps) = NaN; Omieback(1:nsteps) = NaN; NOmieback(1:nsteps) = NaN; NO2mieback(1:nsteps) = NaN;
-%         O3mieforw(1:nsteps) = NaN; Omieforw(1:nsteps) = NaN; NOmieforw(1:nsteps) = NaN; NO2mieforw(1:nsteps) = NaN;
+        Nmie(1:nsteps,1:4) = NaN;
         P(1:nsteps,1:4) = NaN;
         L(1:nsteps,1:4) = NaN;
-        %% First stage
+        %% Step 1
         % Initialize Backward Euler concentrations and maximum Backward Euler concentrations for each
         % active species with concentrations from the beginning of the simulation
-        O3mie(1) = O3; Omie(1) = O; NOmie(1) = NO; NO2mie(1) = NO2;
-        O3miemax = O3; Omiemax = O; NOmiemax = NO; NO2miemax = NO2;
-        O3mieback = O3; Omieback = O; NOmieback = NO; NO2mieback = NO2;
-        %% Second stage
+        Nmiemax = [NO2 NO O O3];
+        Nmieback = [NO2 NO O O3];
+        Nmie(1,:) =  [NO2 NO O O3];
+        %% Step 2
         % Estimate reaction rates by multiplying rate coeffcients by Backward Euler concentrations
+        % Skipped
 %         R1 = k1*NOmieback(1)*O3mieback(1);
 %         R2 = J*NO2mieback(1);
 %         R3 = k3*Omieback(1);
-        %% Third stage
+        %% Step 3
+        % Estimate production rates and implicit loss coef?cients for each species
         % Time loop
         for i = 2:nsteps
-            disp(['i = ' num2str(i)])
+%             disp(['i = ' num2str(i) '; Time: ' num2str(t0+dt*(i-1)) 's'])
             % Iteration loop
             m = 0;
             np = 0;
             while np <= Np
-                %disp(['np = ' num2str(np)])
                 m = m + 1;
-                P(i,1) = k1*NOmieback*O3mieback; % NO2
-                P(i,2) = J*NO2mieback; % NO
-                P(i,3) = J*NO2mieback; % O
-                P(i,4) = k3*Omieback; % O3
-                L(i,1) = J; % NO2, L - implicit loss coefficient (lambda)
-                L(i,2) = k1*O3mieback; % NO
-                L(i,3) = k3; % O
-                L(i,4) = k1*NOmieback; % O3
-                NO2mieback = (NO2mie(i-1) + dt*P(i,1))/(1 + dt*L(i,1));
-                NO2mieforw = NO2mie(i-1) + dt*(P(i,1) - L(i,1));
-                % Check convergence
-                if any(NO2mieforw < 0) % || NOmieforw < 0 || Omieforw < 0 || O3mieforw < 0)
-                    np = 0;
-                else
-                    np = np +1;
+                if m > itermax
+                    error(['Iterations do not converge for the time step ' num2str(i)])
                 end
-                %% Six stage
-                % Limit current backward Euler concentrations
-                NO2mieback = min(NO2mieback, NO2miemax);
-                % Update maximum backward Euler concentrations
-                NO2miemax = max(NO2mieback, NO2mie(i-1));
+                % Production rates for each species
+                P(i,1) = k1*Nmieback(2)*Nmieback(4); % NO2
+                P(i,2) = J*Nmieback(1); % NO
+                P(i,3) = J*Nmieback(1); % O
+                P(i,4) = k3*Nmieback(3); % O3
+                % Implicit loss coef?cients for each species
+                L(i,1) = J; % NO2
+                L(i,2) = k1*Nmieback(4); % NO
+                L(i,3) = k3; % O
+                L(i,4) = k1*Nmieback(2); % O3
+                %% Step 4 and 5
+                % Calculate backward and forward Euler concentrations for all species at iteration m +1
+                for s = 1:4
+                    Nmieback(s) = (Nmie(i-1,s) + dt*P(i,s))/(1 + dt*L(i,s));
+                    Nmieforw(s) = Nmie(i-1,s) + dt*(P(i,s) - L(i,s));
+                end
+                %% Step 6
+                % Check convergence
+                if all(Nmieforw(:) >= 0)
+                    np =np +1;
+                else
+                    np = 0;
+                end
+                if i > 2 % convergence occurs only without checking on the first time step in a loop
+                    for s = 1:4
+                        % Limit current backward Euler concentrations
+                        Nmieback(s) = min(Nmieback(s), Nmiemax(s));
+                        % Update maximum backward Euler concentrations
+                        Nmiemax(s) = max(Nmieback(s), Nmie(i-1,s));
+                    end
+                end
             end
-            %% Seventh stage
-            NO2mie(i) = NO2mieback;
+            %% Step 7
+            % The ?nal concentrations
+            Nmie(i,:) = Nmieback(:);
         end
 end
 %% Plot results
@@ -112,18 +126,24 @@ switch scheme
     case 1
         figure;
         end1 = 20;
-        subplot(2,2,1); plot(O3forw(1:end1),'b'); title('O3')
-        subplot(2,2,2); plot(Oforw(1:end1),'b'); title('O')
-        subplot(2,2,3); plot(NOforw(1:end1),'b'); title('NO')
-        subplot(2,2,4); plot(NO2forw(1:end1),'b'); title('NO2')
+        subplot(2,2,1); plot(O3forw(1:end1),'k'); title('O3')
+        subplot(2,2,2); plot(Oforw(1:end1),'k'); title('O')
+        subplot(2,2,3); plot(NOforw(1:end1),'k'); title('NO')
+        subplot(2,2,4); plot(NO2forw(1:end1),'k'); title('NO2')
         %subplot(2,2,1); plot(t0:dt:nt,O3forw(1:end-1),'b'); title('O3')
         %subplot(2,2,2); plot(t0:dt:nt,Oforw(1:end-1),'b'); title('O')
         %subplot(2,2,3); plot(t0:dt:nt,NOforw(1:end-1),'b'); title('NO')
         %subplot(2,2,4); plot(t0:dt:nt,NO2forw(1:end-1),'b'); title('NO2')
     case 2
         figure;
-        subplot(2,2,1); plot(O3back(1:end-1),'b'); title('O3')
-        subplot(2,2,2); plot(Oback(1:end-1),'b'); title('O')
-        subplot(2,2,3); plot(NOback(1:end-1),'b'); title('NO')
-        subplot(2,2,4); plot(NO2back(1:end-1),'b'); title('NO2')
+        subplot(2,2,1); plot(NO2back(1:end-1),'b'); title('NO2')
+        subplot(2,2,2); plot(NOback(1:end-1),'b'); title('NO')
+        subplot(2,2,3); plot(Oback(1:end-1),'b'); title('O')
+        subplot(2,2,4); plot(O3back(1:end-1),'b'); title('O3')
+    case 3
+        figure;
+        subplot(2,2,1); plot(Nmie(1:end-1,1),'r'); title('NO2'); hold on; plot(NO2back(1:end-1),'b'); title('NO2')
+        subplot(2,2,2); plot(Nmie(1:end-1,2),'r'); title('NO'); hold on; plot(NOback(1:end-1),'b'); title('NO')
+        subplot(2,2,3); plot(Nmie(1:end-1,3),'r'); title('O'); hold on; plot(Oback(1:end-1),'b'); title('O')
+        subplot(2,2,4); plot(Nmie(1:end-1,4),'r'); title('O3'); hold on; plot(O3back(1:end-1),'b'); title('O3')
 end
