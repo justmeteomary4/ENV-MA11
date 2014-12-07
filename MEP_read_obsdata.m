85c78e52ca7c8ccfc7115a671aa419da66e59377
maria.zamyatina <maria.zamyatina.int@gmail.com>
2014-12-07 01:46:03 +0000

    Plotting block is corrected

--- MEP_read_obsdata.m
+++ MEP_read_obsdata.m
@@ -75,13 +75,17 @@ switch plottype
     case 2
         for i = 1:ndays
             figure(109);
-            subplot(2,2,1); plot(NO2((ibegc2+1440*(i-1)):(ibegc2+1439*i)),'color',cvec(i,:)); %hold on;
+            metind = ibegm+144*(i-1):ibegm+144*i-1; % for meteo
+            chemind1 = ibegc1+1440*(i-1):ibegc1+1440*i-1; % for jNO2
+            chemind2 = ibegc2+1440*(i-1):ibegc2+1440*i-1; % for NO2, NO, O3
+            
+            subplot(2,2,1); plot(NO2(chemind2),'color',cvec(i,:)); %hold on;
             title('NO2'); xlabel('min'); ylabel('ppbv'); xlim([0 1440]); ylim([0 max(NO2(:))]);
-            subplot(2,2,2); plot(NO((ibegc2+1440*(i-1)):(ibegc2+1439*i)),'color',cvec(i,:)); %hold on;
+            subplot(2,2,2); plot(NO(chemind2),'color',cvec(i,:)); %hold on;
             title('NO'); xlabel('min'); ylabel('ppbv'); xlim([0 1440]); ylim([0 max(NO(:))]);
-            subplot(2,2,3); plot(jNO2((ibegc1+1440*(i-1)):(ibegc1+1439*i)),'color',cvec(i,:)); %hold on;
+            subplot(2,2,3); plot(jNO2(chemind1),'color',cvec(i,:)); %hold on;
             title('jNO2'); xlabel('min'); ylabel('1/s'); xlim([0 1440]); ylim([0 max(jNO2(:))]); 
-            subplot(2,2,4); plot(O3((ibegc2+1440*(i-1)):(ibegc2+1439*i)),'color',cvec(i,:)); %hold on;
+            subplot(2,2,4); plot(O3(chemind2),'color',cvec(i,:)); %hold on;
             title('O3'); xlabel('min'); ylabel('ppbv'); xlim([0 1440]); ylim([0 max(O3(:))]);
             imgname= strcat(outdir,'/pics_obsdata_chem/','obsdata_chem_',datestr([2004,dates(2,i),dates(1,i),0,0,0],'mmm dd'),'.png');
             suptitle(datestr([2004,dates(2,i),dates(1,i),0,0,0],'mmm dd'));
@@ -89,13 +93,13 @@ switch plottype
             print(gcf,'-dpng','-r300',imgname);
             
             figure(110);
-            subplot(2,2,1); plot(ta((ibegm+144*(i-1)):(ibegm+143*i)),'color',cvec(i,:));
+            subplot(2,2,1); plot(ta(metind),'color',cvec(i,:));
             title('Air temperature'); xlabel('min'); ylabel('degrees Celsius'); xlim([0 144]); ylim([min(ta(:)) max(ta(:))]);
-            subplot(2,2,2); plot(ps((ibegm+144*(i-1)):(ibegm+143*i)),'color',cvec(i,:));
+            subplot(2,2,2); plot(ps(metind),'color',cvec(i,:));
             title('Surface air pressure'); xlabel('min'); ylabel('hPa'); xlim([0 144]); ylim([min(ps(:)) max(ps(:))]);
-            subplot(2,2,3); plot(wdir((ibegm+144*(i-1)):(ibegm+143*i)),'color',cvec(i,:));
+            subplot(2,2,3); plot(wdir(metind),'color',cvec(i,:));
             title('Wind direction'); xlabel('min'); ylabel('degrees'); xlim([0 144]); ylim([0 360]);
-            subplot(2,2,4); plot(wspeed((ibegm+144*(i-1)):(ibegm+143*i)),'color',cvec(i,:));
+            subplot(2,2,4); plot(wspeed(metind),'color',cvec(i,:));
             title('Wind speed'); xlabel('min'); ylabel('m/s'); xlim([0 144]); ylim([min(wspeed(:)) max(wspeed(:))]);
             imgname= strcat(outdir,'/pics_obsdata_meteo/','obsdata_meteo_',datestr([2004,dates(2,i),dates(1,i),0,0,0],'mmm dd'),'.png');
             suptitle(datestr([2004,dates(2,i),dates(1,i),0,0,0],'mmm dd'));
